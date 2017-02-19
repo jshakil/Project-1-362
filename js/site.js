76d82947b042cb495bee8b694e7f7644e9b847b3
Jabran Shakil <jabranshakil@live.com>
2017-02-18 19:39:05 -0600

    Added in a jQuery function that tests for an on click event on the submit button

--- js/site.js
+++ js/site.js
@@ -1,3 +1,9 @@
 //Make jQuery lose control of the $
 $.noConflict();
 
+jQuery(function($) {
+  $('#user-form').on('submit', function(event) {
+      console.log('hello');
+      $('#test').append('hello');
+  })
+});
