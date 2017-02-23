aa856a82ad359fa1e3b25c9e80d5627e428b840c
Jabran Shakil <jabranshakil@live.com>
2017-02-23 17:26:52 -0600

    Added for loop in the JavaScript to output an alert

--- js/site.js
+++ js/site.js
@@ -5,5 +5,17 @@ jQuery(function($) {
   $('#user-form').on('submit', function(event) {
       console.log('hello');
       $('#test').append('hello');
-  })
+      
+      var emailInput = $('#email').val()
+      console.log(emailInput)
+      
+      for(count = 0; count < emailInput.length; count++){
+          if (count == '@'){
+              alert('looks good bro!')
+          }
+      }
+      
+      event.preventDefault();
+  });
 });
+
