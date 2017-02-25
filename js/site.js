58b78a729cc2e0a01bc865970459fe657c490dfc
Jabran Shakil <jabranshakil@live.com>
2017-02-25 11:52:24 -0600

    Removed the test code of lines

--- js/site.js
+++ js/site.js
@@ -3,18 +3,15 @@ $.noConflict();
 
 jQuery(function($) {
   $('#user-form').on('submit', function(event) {
-      console.log('hello');
-      $('#test').append('hello');
       
       var emailInput = $('#email').val()
       console.log(emailInput)
       
       for(count = 0; count < emailInput.length; count++){
           if (count == '@'){
-              alert('looks good bro!')
+              window.alert("looks good bro!");
           }
       }
-      
       event.preventDefault();
   });
 });
