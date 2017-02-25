57db37d70cd3f7fe63f60b68ea671d0efc87a2d3
Jabran Shakil <jabranshakil@live.com>
2017-02-25 12:50:48 -0600

    Cleaned up JS with the linter

--- js/site.js
+++ js/site.js
@@ -4,15 +4,11 @@ $.noConflict();
 jQuery(function($) {
   $('#user-form').on('submit', function(event) {
       
-      var emailInput = $('#email').val()
-      console.log(emailInput)
+    var emailInput = $('#email').val()
+    console.log(emailInput)
       
-      for(count = 0; count < emailInput.length; count++){
-          if (count == '@'){
-              window.alert("looks good bro!");
-          }
-      }
-      event.preventDefault();
+    event.preventDefault();
+      
   });
 });
 
